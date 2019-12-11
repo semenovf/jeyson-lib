dbcb3ee73aad5316fd18797746179c06db1d5d2a
Semenov Fedor <fedor.v.semenov@gmail.com>
2019-12-11 15:53:09 +0500

    Updated .travis.yml (added support for Coveralls)

--- build_and_test.sh
+++ build_and_test.sh
@@ -8,6 +8,6 @@ uname -a
 $CXX --version
 
 mkdir -p build && cd build
-cmake -GNinja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
+cmake -GNinja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release ${CMAKE_OPTIONS} .
 cmake --build . && ctest --output-on-failure
 cd ..
