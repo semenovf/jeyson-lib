fdb9f5ecae5de9eab2db0eeb6d40f4621b8df914
Semenov Fedor <fedor.v.semenov@gmail.com>
2019-12-11 15:39:35 +0500

    Updated .travis.yml

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
+
+if [[ "${COMPILER}" != "" ]]; then
+    export CXX=${COMPILER};
+fi
+
+uname -a
+$CXX --version
+
+mkdir -p build && cd build
+cmake -GNinja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
 cmake --build . && ctest --output-on-failure
+cd ..
