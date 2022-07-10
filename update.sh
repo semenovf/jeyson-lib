78022e73ffecfc3dadeee548cb2717fbf0f406de
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 18:36:07 +0500

    Updated `update.sh`.

--- update.sh
+++ update.sh
@@ -5,14 +5,14 @@ JANNSON_RELEASE=v2.14
 
 if [ -e .git ] ; then
 
-    git pull \
+    git checkout master && git pull origin master \
         && git submodule update --init \
         && git submodule update --remote \
         && cd 3rdparty/portable-target && git checkout master && git pull \
         && cd $CWD \
-        && cd 3rdparty/pfs/common && git checkout master && git pull  \
+        && cd 3rdparty/pfs/common && ./update.sh  \
         && cd $CWD \
-        && cd 3rdparty/rocksdb && git checkout $JANNSON_RELEASE
+        && cd 3rdparty/jansson && git checkout $JANNSON_RELEASE
 
 fi
 
