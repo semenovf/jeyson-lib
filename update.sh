41c385dbbcbc980eb6d334e009ea60b72f1d86e3
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 14:24:37 +0500

    Added `update.sh` script for update dependencies.

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CWD=`pwd`
+JANNSON_RELEASE=v2.14
+
+if [ -d .git ] ; then
+
+    git pull \
+        && git submodule update --init \
+        && git submodule update --remote \
+        && cd 3rdparty/portable-target && git checkout master && git pull \
+        && cd $CWD \
+        && cd 3rdparty/pfs/common && git checkout master && git pull  \
+        && cd $CWD \
+        && cd 3rdparty/rocksdb && git checkout $JANNSON_RELEASE
+
+fi
+
