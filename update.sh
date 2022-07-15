e64f3d000bdbc6615c7e55fb9d90e47598f95c03
Fedor Semenov <fedor.v.semenov@gmail.com>
2022-07-15 13:02:31 +0500

    Updated `update.sh` and `build-windows.sh`.

--- update.sh
+++ update.sh
@@ -5,11 +5,11 @@ JANNSON_RELEASE=v2.14
 
 if [ -e .git ] ; then
 
-    git checkout master && git pull origin master \
-        && git submodule update --init --recursive \
-        && cd 3rdparty/portable-target && git checkout master && git pull origin master \
-        && cd $CWD \
-        && cd 3rdparty/pfs/common && git checkout master && git pull origin master \
+    #git checkout master && git pull origin master \
+    #    && 
+        git submodule update --init --recursive \
+        && git submodule update --init --recursive --remote -- 3rdparty/portable-target \
+        && git submodule update --init --recursive --remote -- 3rdparty/pfs/common \
         && cd $CWD \
         && cd 3rdparty/jansson && git checkout $JANNSON_RELEASE
 
