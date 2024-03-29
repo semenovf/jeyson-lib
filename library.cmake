3276ff5fcfffb0e95d3c84f6c3e5331cdc6954a9
Vladislav Trifochkin <demoversion@mail.ru>
2024-03-29 21:58:20 +0500

    fix: Refactored build scripts.

--- library.cmake
+++ library.cmake
@@ -1,11 +1,12 @@
 ################################################################################
-# Copyright (c) 2019-2023 Vladislav Trifochkin
+# Copyright (c) 2019-2024 Vladislav Trifochkin
 #
 # This file is part of `jeyson-lib`.
 #
 # Changelog:
 #      2022.02.07 Initial version.
 #      2023.02.10 Separated static and shared builds.
+#      2024.03.29 Replaced the sequence of two target configurations with a foreach statement.
 ################################################################################
 cmake_minimum_required (VERSION 3.11)
 project(jeyson C CXX)
@@ -20,53 +21,30 @@ endif()
 
 if (JEYSON__BUILD_SHARED)
     portable_target(ADD_SHARED ${PROJECT_NAME} ALIAS pfs::jeyson EXPORTS JEYSON__EXPORTS)
+    list(APPEND _jeyson__targets ${PROJECT_NAME})
 endif()
 
 if (JEYSON__BUILD_STATIC)
     set(STATIC_PROJECT_NAME ${PROJECT_NAME}-static)
     portable_target(ADD_STATIC ${STATIC_PROJECT_NAME} ALIAS pfs::jeyson::static EXPORTS JEYSON__STATIC)
+    list(APPEND _jeyson__targets ${STATIC_PROJECT_NAME})
 endif()
 
 if (NOT TARGET pfs::common)
-    portable_target(INCLUDE_PROJECT
-        ${PORTABLE_TARGET__CURRENT_PROJECT_DIR}/3rdparty/pfs/common/library.cmake)
+    portable_target(INCLUDE_PROJECT ${CMAKE_CURRENT_LIST_DIR}/2ndparty/common/library.cmake)
 endif()
 
-if (JEYSON__ENABLE_JANSSON AND NOT TARGET jansson)
-    if (NOT JEYSON__JANSSON_ROOT)
-        set(JEYSON__JANSSON_ROOT "${PORTABLE_TARGET__CURRENT_PROJECT_DIR}/3rdparty/jansson" CACHE INTERNAL "")
-    endif()
-
-    portable_target(INCLUDE_PROJECT
-        ${PORTABLE_TARGET__CURRENT_PROJECT_DIR}/cmake/Jansson.cmake)
-
-    if (JEYSON__BUILD_SHARED)
-        portable_target(INCLUDE_DIRS ${PROJECT_NAME} PRIVATE $<TARGET_PROPERTY:jansson,INCLUDE_DIRECTORIES>)
-    endif()
-
-    if (JEYSON__BUILD_STATIC)
-        portable_target(INCLUDE_DIRS ${STATIC_PROJECT_NAME} PRIVATE $<TARGET_PROPERTY:jansson,INCLUDE_DIRECTORIES>)
-    endif()
-
+if (JEYSON__ENABLE_JANSSON)
     list(APPEND _jeyson__sources ${CMAKE_CURRENT_LIST_DIR}/src/jansson.cpp)
 endif()
 
-if (JEYSON__BUILD_SHARED)
-    portable_target(SOURCES ${PROJECT_NAME} ${_jeyson__sources})
-    portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-    portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::common)
+foreach(_target IN LISTS _jeyson__targets)
+    portable_target(SOURCES ${_target} ${_jeyson__sources})
+    portable_target(INCLUDE_DIRS ${_target} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+    portable_target(LINK ${_target} PUBLIC pfs::common)
 
     if (JEYSON__ENABLE_JANSSON)
-        portable_target(LINK ${PROJECT_NAME} PRIVATE jansson)
+        portable_target(LINK ${_target} PRIVATE jansson)
+        portable_target(INCLUDE_DIRS ${_target} PRIVATE $<TARGET_PROPERTY:jansson,INCLUDE_DIRECTORIES>)
     endif()
-endif()
-
-if (JEYSON__BUILD_STATIC)
-    portable_target(SOURCES ${STATIC_PROJECT_NAME} ${_jeyson__sources})
-    portable_target(INCLUDE_DIRS ${STATIC_PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-    portable_target(LINK ${STATIC_PROJECT_NAME} PUBLIC pfs::common)
-
-    if (JEYSON__ENABLE_JANSSON)
-        portable_target(LINK ${STATIC_PROJECT_NAME} PRIVATE jansson)
-    endif()
-endif()
+endforeach()
